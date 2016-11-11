36f2e181c1b5975a98061c81526abd49ccd5dc3b
NielsJustesen <niels449@hotmail.com>
2016-11-11 12:56:33 +0100

    New repo

---
+++ Assets/ButtonPush.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class ButtonPush : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    private Image buttonImg;
+    [SerializeField] private TaskManager taskManager;
+    // Use this for initialization
+    void Start()
+    {
+        buttonImg = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Vector2 pos;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(buttonImg.rectTransform, eventData.position, eventData.pressEventCamera, out pos))
+        {
+            buttonImg.color = Color.red;
+            taskManager.CheckforObjective(new Vector3(0,0,0));
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (buttonImg.color == Color.red)
+        {
+            buttonImg.color = Color.white;
+            taskManager.objectiveCounter++;
+        }
+    }
+}
---
+++ Assets/Scripts/TBone.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TBone : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Car;
+    [SerializeField]
+    private GameObject playerVehicle;
+    float minSpeed;
+    float maxSpeed = 1;
+    float actualSpeed;
+    bool canSpawn = true;
+    float ready = 0;
+	void Update ()
+    {
+        if (!canSpawn)
+        {
+            ready += Time.deltaTime;
+            if (ready > 4)
+            {
+                canSpawn = true;
+                ready = 0;
+            }
+        }
+	}
+
+    public void CreateTBoner()
+    {
+        GameObject tBoner = Instantiate(Car);
+        tBoner.transform.position = new Vector3(this.transform.position.x -6, this.transform.position.y + 1, this.transform.position.z + 7);
+        Debug.Log("T-Boner spawned");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        minSpeed = playerVehicle.GetComponent<CarController>().CurrentSpeed;
+        if (minSpeed > maxSpeed && canSpawn)
+        {
+            CreateTBoner();
+            canSpawn = false;
+        }
+        Debug.Log("The minSpeed was: " + minSpeed);
+    }
+}
