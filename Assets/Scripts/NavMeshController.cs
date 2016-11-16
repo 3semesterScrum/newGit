03cc235fb63b75899755f67982db27acf20a876e
NielsJustesen <niels449@hotmail.com>
2016-11-16 14:03:13 +0100

    WalkingNPC Undgå Vehicle test

--- Assets/Scripts/NavMeshController.cs
+++ Assets/Scripts/NavMeshController.cs
@@ -5,19 +5,22 @@ public class NavMeshController : MonoBehaviour
 {
     [SerializeField]
     private GameObject[] targets;
+    private GameObject avoid;
     private NavMeshAgent agent;
     private int i = 0;
     Animator animator;
-
     float yRotation;
     float turn = 0;
+    float rotateNPC = 0;
+    bool vehicleFound = false;
+    bool hasturned = false;
 
     void Start()
     {
-
         agent = this.GetComponent<NavMeshAgent>();
         animator = this.GetComponent<Animator>();
         animator.SetBool("isWalking", true);
+        avoid = GameObject.FindGameObjectWithTag("vehicle");
     }
     void FixedUpdate()
     {
@@ -25,8 +28,33 @@ public class NavMeshController : MonoBehaviour
     }
     void Update()
     {
-        float dist = Vector3.Distance(targets[i].transform.position, transform.position);
+        float avoidDist = Vector3.Distance(avoid.transform.position, transform.position);
+        Debug.Log("AvoidDist: " + avoidDist);
+        AvoidVehicle(avoidDist);
+        if (avoidDist > 8)
+        {
+            float dist = Vector3.Distance(targets[i].transform.position, transform.position);
+            MoveNPC(dist);
+        }
         float rotationDiff = yRotation - transform.rotation.y;
+        AnimateNPC(rotationDiff);
+
+    }
+    public void AvoidVehicle(float avoidDist)
+    {
+        if (avoidDist < 8)
+        {
+            rotateNPC = 1;
+            transform.Rotate(new Vector3(0, rotateNPC));
+            if (transform.rotation.y > 45)
+            {
+                rotateNPC = -1;
+            }
+            transform.Translate(new Vector3(0, 0, 2) * Time.deltaTime);
+        }
+    }
+    public void AnimateNPC(float rotationDiff)
+    {
         if (rotationDiff > 0.005f || rotationDiff < -0.005f)
         {
             if (yRotation < transform.rotation.y)
@@ -66,7 +94,12 @@ public class NavMeshController : MonoBehaviour
             }
         }
         animator.SetFloat("BlendNPC", turn);
+
+    }
+    public void MoveNPC(float dist)
+    {
         agent.SetDestination(targets[i].transform.position);
+
         if (dist < 2)
         {
             i++;
@@ -75,6 +108,5 @@ public class NavMeshController : MonoBehaviour
         {
             i = 0;
         }
-
     }
 }
