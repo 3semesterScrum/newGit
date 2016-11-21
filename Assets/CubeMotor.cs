068ecdece7b5b8500a28ee19898315ee08f4f179
DESKTOP-KHDRP1R\Frederik <ninjakongenfh@yahoo.com>
2016-11-21 10:56:06 +0100

    Added Mockup models to the different vehicles and made them functional as prefabs

--- Assets/CubeMotor.cs
+++ Assets/CubeMotor.cs
@@ -1,47 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CubeMotor : MonoBehaviour {
+public class CubeMotor : MonoBehaviour
+{
 
-    public float moveSpeed = 5.0f;
-    public float drag = 0.1f;
-    public float terminalRotationSpeed = 25.0f;
     public Vector3 MoveVector { set; get; }
     public VirtualJoystick joystick;
     private CarController car;
 
     private Rigidbody thisRigidbody;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         car = GetComponent<CarController>();
-        thisRigidbody = gameObject.AddComponent<Rigidbody>();
-        //thisRigidbody.maxAngularVelocity = terminalRotationSpeed;
-        //thisRigidbody.drag = drag;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         MoveVector = PoolInput();
-        if(car == null)
+        if (MoveVector.z == 0 && MoveVector.x == 0)
         {
-            Move();
+            car.Move(MoveVector.x, MoveVector.z, 1f, 0f);
         }
         else
         {
-            if(MoveVector.z == 0 && MoveVector.x == 0)
-            {
-                car.Move(MoveVector.x, MoveVector.z, 1f, 0f);
-            }
-            else
-            {
-                car.Move(MoveVector.x, MoveVector.z, MoveVector.z, 0f);
-            }
+            car.Move(MoveVector.x, MoveVector.z, MoveVector.z, 0f);
         }
-	}
-
-    private void Move()
-    {
-        thisRigidbody.AddForce(MoveVector * moveSpeed);
     }
 
     private Vector3 PoolInput()
@@ -50,7 +35,7 @@ public class CubeMotor : MonoBehaviour {
 
         dir.x = joystick.Horizontal();
         dir.z = joystick.Vertical();
-        if (dir.magnitude>1)
+        if (dir.magnitude > 1)
         {
             dir.Normalize();
         }
